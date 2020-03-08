a007aa10c0f7bcbec8703a408c5abb2fd4ac177d
LAFI KAMEL <50940791+nirajkumar1428@users.noreply.github.com>
2020-03-08 04:40:11 -0700

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          CI-CD demo for s3 bucket
         </a>
       </header>
     </div>
